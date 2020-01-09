4c9f22e59249f2ef6ac2e4a776d978a1c0643eac
pb-totoro <jonesec2@gmail.com>
2020-01-08 23:25:20 -0500

    Added cost logic:

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,36 @@
-var resource = []
+var resource = [Number(2000)]
+$('#resource').html("Available Resources: " + parseInt(resource) + " points")
 
-
-$('#buyMore').on("click", function() {
+$('#buyButton').on("click", function () {
     var amount = prompt("How much gold will you spend to add resources?")
-    console.log(amount)
+    resource = Number(resource) + Number(amount)
+
+    $('#resource').html("Available Resources: " + parseInt(resource) + " points")
+})
+
+//getting undefined
+// try for each
+$('.check').on("click", function() {
+    var r = confirm("Are you sure?")
+    if (r == true) {
+        console.log("yes")
+        var cost = $("#cost").attr("data-number")
+        resource = Number(resource) - Number(cost)
+        $('#resource').html("Available Resources: " + parseInt(resource) + " points")
+    }
+    else { console.log("canceled") }
 })
 
 
+// $('#cost').on("click", function() {
+//     var cost = $("button").data("number")
+//     console.log(cost)
+//     confirm("Are you sure?")
+//     if (true) {
+//         var cost = $("button").data("number")
+//         console.log(cost)
+//     }
+//     else { return }
+// })
 
-$('#resource').html("Available Resources: " + resource)
+
