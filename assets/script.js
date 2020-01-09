f7d03ad5e49d9698879df7a33148fdad594176ec
pb-totoro <jonesec2@gmail.com>
2020-01-08 21:54:41 -0500

    Added prompt

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
-var resource = 1400
+var resource = []
+
+
+$('#buyMore').on("click", function() {
+    var amount = prompt("How much gold will you spend to add resources?")
+    console.log(amount)
+})
+
+
 
 $('#resource').html("Available Resources: " + resource)
-
-$
